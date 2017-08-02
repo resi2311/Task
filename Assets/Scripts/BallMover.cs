5a4c64ed8a53bdcfc2697197eb13877937d9601a
resi2311 <resi2311@mail.ru>
2017-08-02 07:01:39 +0300

    Init

---
+++ Assets/Scripts/BallMover.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class BallMover : MonoBehaviour
+{
+	Vector3 speed;
+	Vector3[] path_phys;
+	float speedScalar;
+	int targetIdx;
+	private bool IsOnPath;
+	string pathToJson;
+	string jsonString;
+
+	// Use this for initialization
+	void Start ()
+	{
+		IsOnPath = false;
+		targetIdx = 0;
+		LoadPath ();
+		transform.position = path_phys [0];
+		speedScalar = 1;
+	}
+
+	void LoadPath ()
+	{
+		pathToJson = Application.streamingAssetsPath + "/ball_path.json";
+		jsonString = File.ReadAllText (pathToJson);
+		Path path = JsonUtility.FromJson<Path> (jsonString);
+		path_phys = new Vector3[path.x.Length];
+		for (int i = 0; i < path.x.Length; i++) 
+		{
+			path_phys[i] = new Vector3 (path.x [i], path.y [i], path.z [i]);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (IsOnPath) 
+		{
+			if ((path_phys[targetIdx] - transform.position).magnitude < 0.01) 
+			{
+				
+
+				if(targetIdx == path_phys.Length-1)
+				{
+					transform.position = path_phys[targetIdx];
+					IsOnPath = false;
+					return;
+				}
+				speed = (path_phys [targetIdx+1] - path_phys [targetIdx]).normalized;
+				targetIdx++;
+				transform.position = path_phys[targetIdx-1];
+				return;
+			}
+			transform.position += speed * speedScalar * Time.deltaTime;
+		}
+	}
+
+	void OnMouseDown()
+	{
+		if (!IsOnPath) 
+		{
+			IsOnPath = true;
+			transform.position = path_phys [0];
+			targetIdx = 0;
+		}
+	}
+
+
+}
+
+     
+
+[System.Serializable]
+public class Path
+{
+	public float[] x;
+	public float[] y;
+	public float[] z;
+}
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	private Vector3 offset;
+	public GameObject Ball;
+
+	// Use this for initialization
+	void Start () 
+	{
+		offset = transform.position - Ball.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () 
+	{
+		transform.position = Ball.transform.position + offset;
+	}
+}
